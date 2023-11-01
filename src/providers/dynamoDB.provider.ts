39dbd5e44c4c24aca24a7e33ad5ab53461ed6247
Alvaro Javier Perez Salcedo <japersa92@gmail.com>
2023-11-01 18:52:39 -0500

    fix

--- src/providers/dynamoDB.provider.ts
+++ src/providers/dynamoDB.provider.ts
@@ -6,8 +6,18 @@ const marshallOptions = {
   convertClassInstanceMap: true,
 };
 
+const configurationDynamo = 
+  process.env.IS_OFFLINE ?
+  {
+    region: 'eu-west-1',
+    endpoint: 'http://localhost:8000',
+    accessKeyId: 'AKIASAS7VYOCDPIZUXMN',
+    secretAccessKey: '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'
+  } :
+  {};
+
 
 export const DynamoDBProvider = {
   provide: 'DYNAMODB_CONNECTION',
-  useFactory: () => DynamoDBDocument.from(new DynamoDB({}), { marshallOptions }),
+  useFactory: () => DynamoDBDocument.from(new DynamoDB(configurationDynamo), { marshallOptions }),
 };
